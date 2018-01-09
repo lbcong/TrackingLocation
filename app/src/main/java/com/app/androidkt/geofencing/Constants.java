595465609ee8784f51a4764f03ea67f057a94e35
Your Name <you@example.com>
2018-01-09 11:49:48 +0700

    init

---
+++ app/src/main/java/com/app/androidkt/geofencing/Constants.java
@@ -0,0 +1,27 @@
+package com.app.androidkt.geofencing;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.HashMap;
+
+/**
+ * Created by brijesh on 15/4/17.
+ */
+
+public class Constants {
+
+
+    public static final String GEOFENCE_ID_STAN_UNI = "STAN_UNI";
+    public static final float GEOFENCE_RADIUS_IN_METERS = 100;
+
+    /**
+     * Map for storing information about stanford university in the Stanford.
+     */
+    public static final HashMap<String, LatLng> AREA_LANDMARKS = new HashMap<String, LatLng>();
+
+    static {
+        // stanford university.
+        AREA_LANDMARKS.put(GEOFENCE_ID_STAN_UNI, new LatLng(37.427476, -122.170262));
+        AREA_LANDMARKS.put(GEOFENCE_ID_STAN_UNI, new LatLng(16.068743, 108.148553));
+    }
+}
---
+++ app/src/main/java/com/app/androidkt/geofencing/GeofenceRegistrationService.java
@@ -0,0 +1,38 @@
+package com.app.androidkt.geofencing;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import java.util.List;
+
+public class GeofenceRegistrationService extends IntentService {
+
+    private static final String TAG = "GeoIntentService";
+
+    public GeofenceRegistrationService() {
+        super(TAG);
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        GeofencingEvent geofencingEvent = GeofencingEvent.fromIntent(intent);
+
+        if (geofencingEvent.hasError()) {
+            Log.d(TAG, "GeofencingEvent error " + geofencingEvent.getErrorCode());
+        } else {
+            int transaction = geofencingEvent.getGeofenceTransition();
+            List<Geofence> geofences = geofencingEvent.getTriggeringGeofences();
+            Geofence geofence = geofences.get(0);
+            if (transaction == Geofence.GEOFENCE_TRANSITION_ENTER && geofence.getRequestId().equals(Constants.GEOFENCE_ID_STAN_UNI)) {
+                Log.d(TAG, "Ban dang o? trong vong` ban kinh");
+            } else {
+                Log.d(TAG, "Ban dang o? ngoai ban kinh");
+            }
+        }
+    }
+}
